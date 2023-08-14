5c17c8034a515e9cf3fd28a5bcbbe887bc6e19a8
Gabriel Viegas <gabrielgl13@hotmail.com>
2023-08-13 23:00:49 -0300

    feat: add typeorm and postgres

--- src/infrastructure/typeorm/typeorm.module.ts
+++ src/infrastructure/typeorm/typeorm.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { EnvironmentConfigService } from '../environment-config/environment-config.service';
-// import { createTypeOrmModuleOptions } from 'src/shared/factories/create-typeorm-factory';
+import { EnvironmentConfigService } from '../environment-config/environment-config.service';
+import { createTypeOrmModuleOptions } from 'src/shared/factories/create-typeorm-factory';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRootAsync({
-    //   imports: [],
-    //   inject: [],
-    //   // useFactory: [],
-    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [EnvironmentConfigService],
+      inject: [EnvironmentConfigService],
+      useFactory: createTypeOrmModuleOptions,
+    }),
   ],
 })
 export class TypeormModule {}
--- src/shared/factories/create-typeorm-factory.ts
+++ src/shared/factories/create-typeorm-factory.ts
@@ -5,16 +5,16 @@ export const createTypeOrmModuleOptions = (
   config: EnvironmentConfigService,
 ): TypeOrmModuleOptions =>
   ({
-    // type: 'postgres',
-    // host: config.getDatabaseHost(),
-    // port: config.getDatabasePort(),
-    // username: config.getDatabaseUser(),
-    // password: config.getDatabasePassword(),
-    // database: config.getDatabaseName(),
-    // entities: [__dirname + './../../**/*.entity{.ts,.js}'],
-    // synchronize: false,
-    // schema: process.env.DATABASE_SCHEMA,
-    // ssl: {
-    //   rejectUnauthorized: false,
-    // },
+    type: 'postgres',
+    host: config.getDatabaseHost(),
+    port: config.getDatabasePort(),
+    username: config.getDatabaseUser(),
+    password: config.getDatabasePassword(),
+    database: config.getDatabaseName(),
+    entities: [__dirname + './../../**/*.entity{.ts,.js}'],
+    synchronize: false,
+    schema: process.env.DATABASE_SCHEMA,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   } as TypeOrmModuleOptions);
